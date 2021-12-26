f321c32f3dd82f0d7e4c64023aebb4c47d50c39f
Amit <amity552000@gmail.com>
2021-12-26 19:17:43 +0530

    Codes added

---
+++ config.js
@@ -0,0 +1,12 @@
+'use strict'
+module.exports = {
+    mailer: {
+        service: 'Gmail',
+        auth: {
+            user: 'amity552000@gmail.com',
+            pass: 'infiniteloop'
+        }
+    },
+    dbConnstring: 'mongodb://127.0.0.1:27017/codeshare',
+    sessionKey: 'HElloWOrld!'
+}
